15cc3cb05d53c78941630ef3a2b49b7399584b30
Peter Xu <dev@we-sense.org>
2024-10-16 01:09:29 -0400

    1016aa-newstart

--- CG/fromdoc/first_embedding.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-client = OpenAI()
-
-response = client.embeddings.create(
-    model="text-embedding-3-large",
-    input="The food was delicious and the waiter..."
-)
-
-print(response)
